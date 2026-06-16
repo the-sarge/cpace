9a8ba6e1c4baaf3129e4664ab98c060728e51e4e
Joshua Sargent <the-sarge@the-sarge.com>
2026-06-16 13:33:27 -0400

    refactor: deepen length-value encoding
    Signed-off-by: Joshua Sargent <the-sarge@the-sarge.com>

---
+++ length_value.go
@@ -0,0 +1,53 @@
+package cpace
+
+func prependLen(data []byte) []byte {
+	return appendLengthValue(nil, data)
+}
+
+func lvCat(args ...[]byte) []byte {
+	var total int
+	for _, arg := range args {
+		total += lengthValueLen(len(arg))
+	}
+	out := make([]byte, 0, total)
+	for _, arg := range args {
+		out = appendLengthValue(out, arg)
+	}
+	return out
+}
+
+func appendLengthValue(dst, data []byte) []byte {
+	dst = appendLEB128(dst, uint64(len(data)))
+	return append(dst, data...)
+}
+
+func lengthValueLen(n int) int {
+	return leb128Len(uint64(n)) + n
+}
+
+func encodeLEB128(n uint64) []byte {
+	return appendLEB128(make([]byte, 0, leb128Len(n)), n)
+}
+
+func appendLEB128(dst []byte, n uint64) []byte {
+	for {
+		b := byte(n & 0x7f)
+		n >>= 7
+		if n != 0 {
+			b |= 0x80
+		}
+		dst = append(dst, b)
+		if n == 0 {
+			return dst
+		}
+	}
+}
+
+func leb128Len(n uint64) int {
+	out := 1
+	for n >= 0x80 {
+		n >>= 7
+		out++
+	}
+	return out
+}
--- strings.go
+++ strings.go
@@ -2,24 +2,6 @@ package cpace
 
 import "bytes"
 
-func prependLen(data []byte) []byte {
-	out := encodeLEB128(uint64(len(data)))
-	out = append(out, data...)
-	return out
-}
-
-func lvCat(args ...[]byte) []byte {
-	var total int
-	for _, arg := range args {
-		total += len(encodeLEB128(uint64(len(arg)))) + len(arg)
-	}
-	out := make([]byte, 0, total)
-	for _, arg := range args {
-		out = append(out, prependLen(arg)...)
-	}
-	return out
-}
-
 func oCat(a, b []byte) []byte {
 	out := []byte("oc")
 	if lexicographicallyLarger(a, b) {
@@ -44,19 +26,4 @@ func transcriptOC(ya, ada, yb, adb []byte) []byte {
 
 func lexicographicallyLarger(a, b []byte) bool {
 	return bytes.Compare(a, b) > 0
-}
-
-func encodeLEB128(n uint64) []byte {
-	out := make([]byte, 0, 10)
-	for {
-		b := byte(n & 0x7f)
-		n >>= 7
-		if n != 0 {
-			b |= 0x80
-		}
-		out = append(out, b)
-		if n == 0 {
-			return out
-		}
-	}
 }
